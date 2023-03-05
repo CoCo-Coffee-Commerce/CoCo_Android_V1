8367e146b682935f546045c50f1e4d3c2806a071
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2023-03-06 01:11:40 +0900

    Build : Dependency Version 변경

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -27,19 +27,26 @@ object Libraries {
 
     object Application {
         const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
-        const val RUNTIME_LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.RUNTIME_LIFECYCLE}"
-        const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
+        const val RUNTIME_LIFECYCLE =
+            "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.RUNTIME_LIFECYCLE}"
+        const val ACTIVITY_COMPOSE =
+            "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
         const val COMPOSE_UI = "androidx.compose.ui:ui:${Versions.COMPOSE_UI}"
-        const val COMPOSE_UI_PREVIEW_TOOLING = "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE_UI_PREVIEW_TOOLING}"
-        const val COMPOSE_UI_MATERIAL_THREE = "androidx.compose.material3:material3:${Versions.COMPOSE_MATERIAL_THREE}"
+        const val COMPOSE_UI_PREVIEW_TOOLING =
+            "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE_UI_PREVIEW_TOOLING}"
+        const val COMPOSE_UI_MATERIAL_THREE =
+            "androidx.compose.material3:material3:${Versions.COMPOSE_MATERIAL_THREE}"
     }
 
     object Test {
         const val TEST_JUNIT = "junit:junit:${Versions.TEST_JUNIT}"
         const val ANDROID_TEST_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_TEST_JUNIT}"
-        const val ANDROID_ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ANDROID_ESPRESSO_CORE}"
-        const val ANDROID_COMPOSE_UI_TEST_JUNIT = "androidx.compose.ui:ui-test-junit4:${Versions.ANDROID_COMPOSE_UI_TEST_JUNIT}"
+        const val ANDROID_ESPRESSO_CORE =
+            "androidx.test.espresso:espresso-core:${Versions.ANDROID_ESPRESSO_CORE}"
+        const val ANDROID_COMPOSE_UI_TEST_JUNIT =
+            "androidx.compose.ui:ui-test-junit4:${Versions.ANDROID_COMPOSE_UI_TEST_JUNIT}"
         const val DEBUG_UI_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.DEBUG_UI_TOOLING}"
-        const val DEBUG_COMPOSE_TEST_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Versions.DEBUG_COMPOSE_TEST_MANIFEST}"
+        const val DEBUG_COMPOSE_TEST_MANIFEST =
+            "androidx.compose.ui:ui-test-manifest:${Versions.DEBUG_COMPOSE_TEST_MANIFEST}"
     }
 }
