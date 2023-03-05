c2badd6521eef4566e42732c396582618569b7d2
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2023-03-06 01:09:31 +0900

    Delete : 불 필요한 파일 삭제

--- buildSrc/src/main/java/Modules.kt
+++
@@ -1,7 +0,0 @@
-object Modules {
-    const val DOMAIN = ":domain"
-    const val DATA = ":data"
-    const val DI = ":di"
-    const val PRESENTATION = ":presentation"
-    const val ROOT_PROJECT_NAME = "CoCo_Android_Client_V1"
-}
